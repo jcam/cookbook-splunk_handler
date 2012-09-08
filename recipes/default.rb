b6f4364343fd22c191f8537360f54185271b5b29
Greg Albrecht <gba@splunk.com>
2012-09-07 20:11:32 -0700

    pegging chef_gem resource to 2.0.0 of the chef-handler-splunk gem

--- recipes/default.rb
+++ recipes/default.rb
@@ -16,7 +16,9 @@ if (splunk_params['username'] and splunk_params['password'] and
 
   include_recipe 'chef_handler'
 
-  chef_gem 'chef-handler-splunk'
+  chef_gem 'chef-handler-splunk' do
+    version '2.0.0'
+  end
 
   chef_handler 'Chef::Handler::SplunkHandler' do
     action :enable
